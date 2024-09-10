c3ee9d35821af57d6ba85102193198f5f6339e4f
Dario Alvarez <daralv53@gmail.com>
2024-09-10 15:42:58 -0600

    Terminacion

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react"
 import { Activity } from "../types"
+import CalorieDisplay from "./CalorieDisplay"
 
 interface CalorieTrackerProps {
     activities: Activity[]
@@ -18,20 +19,27 @@ export const CalorieTracker = ({activities}: CalorieTrackerProps) => {
         activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0)
     , [activities])
 
+    //Calorias quemadas
+    const netCalories = useMemo(() => caloriesConsumed - caloriesBurned ,[activities])
+
     return (
         <>
             <h2 className="text-4xl font-black text-center text-white">
                 Resumen de calorias
             </h2>
             <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
-                <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-                    <span className="font-black text-6xl text-orange">{caloriesConsumed}</span>
-                    Consumidas
-                </p>
-                <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-                    <span className="font-black text-6xl text-orange">{caloriesBurned}</span>
-                    Quemadas
-                </p>
+                <CalorieDisplay 
+                    calories={caloriesConsumed}
+                    text="Consumidas"
+                />
+                <CalorieDisplay 
+                    calories={caloriesBurned}
+                    text="Ejercicio"
+                />
+                <CalorieDisplay 
+                    calories={netCalories}
+                    text="Diferencia"
+                />
             </div>
         </>
     )
